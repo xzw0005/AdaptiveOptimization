090df08a36f949a6e017dd58c0354d6fb57d2211
Xing Wang <xzw0005@tigermail.auburn.edu>
2016-08-31 13:50:00 -0500

    works

--- SimulatedAnnealing/prod/QAP.py
+++ SimulatedAnnealing/prod/QAP.py
@@ -5,6 +5,7 @@ Created on Aug 29, 2016
 '''
 
 import numpy as np
+import time
 
 class QAP(object):
     '''
@@ -28,7 +29,8 @@ class QAP(object):
     FLOW_MATRIX[13, 1] = 5; FLOW_MATRIX[13, 2] = 5; FLOW_MATRIX[13, 3] = 5; FLOW_MATRIX[13, 4] = 5; FLOW_MATRIX[13, 5] = 5; FLOW_MATRIX[13, 6] = 1; FLOW_MATRIX[13, 10] = 5; FLOW_MATRIX[13, 11] = 3; FLOW_MATRIX[13, 12] = 10; 
     FLOW_MATRIX[14, 2] = 5; FLOW_MATRIX[14, 4] = 5; FLOW_MATRIX[14, 5] = 10; FLOW_MATRIX[14, 8] = 2; FLOW_MATRIX[14, 9] = 5; FLOW_MATRIX[14, 12] = 2; FLOW_MATRIX[14, 13] = 4; 
 
-    def __init__(self, T0, m, n, alpha = 0.9, seed = 12345):
+
+    def __init__(self, T0, m, n, alpha = 0.9, seed = 123456789):
         '''
         Constructor
         '''
@@ -43,57 +45,79 @@ class QAP(object):
         ''' i, j are location indices '''
         return abs(j % 5 - i % 5) + abs(j / 5 - i / 5)
 
-    def costBetweenTwoDepartments(self, i, j):
+    def costBetweenTwoDepartments(self, X, i, j):
         ''' i, j are location indices '''
-        if (i > j):
-            temp = i; i = j; j = temp;
+        #if (i < j):
+        #    temp = i; i = j; j = temp;
         distance = self.computeDistance(i, j)
-        flow = QAP.FLOW_MATRIX[i, j]
+        dept1 = X[i]
+        dept2 = X[j]
+        if (dept1 < dept2):
+            temp = dept1; dept1 = dept2; dept2 = temp;
+        flow = QAP.FLOW_MATRIX[dept1, dept2]
         return flow * distance
     
     def computeTotalCost(self, X):
-        assert X is np.ndarray
+        #assert X is np.ndarray
         totalCost = 0
         for loc1 in np.arange(1, len(QAP.FLOW_MATRIX)):
             for loc2 in np.arange(loc1):
-                totalCost = totalCost + self.costBetweenTwoDepartments(loc1, loc2)
+                totalCost = totalCost + self.costBetweenTwoDepartments(X, loc1, loc2)
         return totalCost
         
     def moveOperator(self, X):
-        #return np.random.shuffle(xOld)
-        locPairs = np.random.choice(len(QAP.FLOW_MATRIX), 2, replace=False)
-        #locPairs.sort()
-        i = locPairs[0]; j = locPairs[1];
-        temp = X[i]
-        X[i] = X[j]
-        X[j] = temp
-        return X
+        Y= np.copy(X)
+        n = np.random.poisson(1, 1)[0] + 1
+        for k in np.arange(n):
+            locPairs = np.random.choice(len(QAP.FLOW_MATRIX), 2, replace=False)
+            i = locPairs[0]; j = locPairs[1];
+            temp = Y[i]
+            Y[i] = Y[j]
+            Y[j] = temp
+        return Y
+    
+#     def moveOperator(self, X):
+#         Y = np.copy(X)
+#         return np.random.permutation(Y)
     
     def simulatedAnnealing(self):
         xOld = self.X0
         costOld = self.computeTotalCost(xOld)
+        print "Initial Solution: ", xOld+1
+        print "Initial Total Cost: ", costOld
         temperature = self.T0
-        m = self.m
-        n = self.n
-        for time in np.arange(m - 1):
-            for iter in range(n - 1):
-                timesWithoutImprovement = 0
+        timesWithoutImprovement = 0
+        for t in np.arange(self.m - 1):
+            for i in range(self.n - 1):
                 xNew = self.moveOperator(xOld)
+                #print xNew
                 costNew = self.computeTotalCost(xNew)
                 deltaCost = costNew - costOld
                 if (deltaCost <= 0):
-                    xOld = xNew
+                    timesWithoutImprovement = 0
+                    xOld = np.copy(xNew)
                     costOld = costNew
                 else:
                     timesWithoutImprovement = timesWithoutImprovement + 1
                     probabilityThreshold = np.exp(-deltaCost / temperature)
                     if (np.random.uniform() <= probabilityThreshold):
-                        xOld = xNew
+                        xOld = np.copy(xNew)
                         costOld = costNew
-                if (timesWithoutImprovement >= 99):
-                    break
+                #print xOld
+                #if (timesWithoutImprovement >= 1e5):
+                #    return xOld
+                #print "Total Cost: ", costOld
             temperature = self.alpha * temperature
-            
+            if (temperature <= 1e-10):
+                break
         return xOld
+
     
-    
+solveQAP = QAP(T0 = 100, m = 500, n = 500, alpha = 0.9, seed = 1)
+print "T0 =", solveQAP.T0, ", m =", solveQAP.m, ", n =", solveQAP.n, ", alpha =", solveQAP.alpha, ", seed ="
+startTime = time.clock()
+res = solveQAP.simulatedAnnealing()  
+print "Best Result Found: ", res+1
+print "Minimum Total Cost Found: ", solveQAP.computeTotalCost(res)
+endTime = time.clock()
+print "Elapsed Time: ", endTime - startTime
